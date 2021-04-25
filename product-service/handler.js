47aa81f6a0867b32a6d0ab89ac3e4967457d9bd8
Maksym_Musiichuk <maksym.musiichuk@gmail.com>
2021-04-25 18:22:21 +0300

    Add env config

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 import { getProducts } from './src/handlers/getProducts';
 import { getProduct } from './src/handlers/getProduct';
 import { addProduct } from './src/handlers/addProduct';
